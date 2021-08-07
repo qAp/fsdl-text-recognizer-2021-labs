8a8fe36cfd196d9c48f7d6f1a74b48a9d3a3ec1a
Jack Yu <jackchungchiehyu@gmail.com>
2021-08-08 02:07:33 +0800

    Residual block
    With 1x1 conv skip connection based on: https://d2l.ai/chapter_convolutional-modern/resnet.html#residual-blocks

--- lab2/text_recognizer/models/cnn.py
+++ lab2/text_recognizer/models/cnn.py
@@ -18,7 +18,13 @@ class ConvBlock(nn.Module):
 
     def __init__(self, input_channels: int, output_channels: int) -> None:
         super().__init__()
-        self.conv = nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(output_channels, output_channels, kernel_size=3, stride=1, padding=1)
+        
+        if input_channels != output_channels:
+            self.use_1x1conv = True
+            self.conv3 = nn.Conv2d(input_channels, output_channels, kernel_size=1, stride=1)
+            
         self.relu = nn.ReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -33,7 +39,13 @@ class ConvBlock(nn.Module):
         torch.Tensor
             of dimensions (B, C, H, W)
         """
-        c = self.conv(x)
+        c = self.conv1(x)
+        r = self.relu(c)
+        c = self.conv2(r)
+        
+        if self.use_1x1conv:
+            c = c + self.conv3(x)
+            
         r = self.relu(c)
         return r
 
